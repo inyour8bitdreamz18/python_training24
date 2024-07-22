bd2705a9766c9ac90ebf99b7e7ed304a0e4e2528
inyour8bitdreamz18 <WaginElla2019>
2024-07-22 15:56:55 +0200

    Создана Фикстура, увеличена читаемость тестов, добавлен Application

--- test_add_group.py
+++ test_add_group.py
@@ -1,70 +1,25 @@
 # -*- coding: utf-8 -*-
-from selenium import wd
-import unittest
+import pytest
 from group import Group
+from application import Application
+
+# Функция, инициализирующая Фикстуру (обязательна метка перед самой функцией)
+@pytest.fixture()
+def app(request):
+    # Создает Фикстуру
+    fixture =Application()
+    # Разрушает Фикстуру
+    request.addfinalizer(fixture.destroy)
+    # Возвращает Фикстуру
+    return  fixture
 
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.wd = wd.Firefox()
-        self.wd.implicitly_wait(60)
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="abcd", header="efg", footer="higk"))
+    app.logout()
 
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/group.php")
-
-    def open_groups_page(self, wd):
-        wd.find_element_by_link_text("groups").click()
-
-    def login(self, wd, username, password):
-        wd.find_element_by_id("LoginForm").click()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def create_group(self, wd, group):
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_groups_page(self, wd):
-        wd.find_element_by_link_text("group page").click()
-
-    def test_add_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
-        self.create_group(wd, Group(name="abcd", header="efg", footer="higk"))
-        self.return_to_groups_page(wd)
-        self.logout(wd)
-
-    def test_add_empty_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
-        self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups_page(wd)
-        self.logout(wd)
-
-    def tearDown(self):
-        self.wd.quit()
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
