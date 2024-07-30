b09086c4406c4ae3887c3fcea22743d2b31218bb
inyour8bitdreamz18 <WaginElla2019>
2024-07-30 14:15:04 +0200

    Выделены вспомогательные методы для заполнения формы контакта и изменения данных в форме контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -6,76 +6,12 @@ class ContactHelper:
         # Ссылка на главный файл фикстуры Application
         self.app = app
 
-    def fill_data(self, contact):
-        wd = self.app.wd
-        # fill name
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        # fill lastname
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        # fill address
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        # fill mobile number
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-        # fill email 1
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        # fill email 2
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").send_keys(contact.email2)
-        # fill email 3
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").send_keys(contact.email3)
-        # fill nickname
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        # fill company
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        # fill title
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        # fill phone number
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").send_keys(contact.home)
-        # fill work
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").send_keys(contact.work)
-        # fill fax
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").send_keys(contact.fax)
-        # fill homepage
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").send_keys(contact.homepage)
-        # fill birth day
-        wd.find_element_by_name("bday").click()
-        wd.find_element_by_name("bday").send_keys(contact.bday)
-        # fill birth month
-        wd.find_element_by_name("bmonth").click()
-        wd.find_element_by_name("bmonth").send_keys(contact.bmonth)
-        # fill birth year
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").send_keys(contact.byear)
-        # fill anniversary day
-        wd.find_element_by_name("aday").click()
-        wd.find_element_by_name("aday").send_keys(contact.aday)
-        # fill anniversary month
-        wd.find_element_by_name("amonth").click()
-        wd.find_element_by_name("amonth").send_keys(contact.amonth)
-        # fill anniversary year
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").send_keys(contact.ayear)
-
-
     def create(self, contact):
         wd = self.app.wd
         # click to add new contact
         wd.find_element_by_link_text("add new").click()
         # init contact creation
-        self.fill_data(contact)
+        self.fill_contact_form(contact)
         # submit contact creation
         wd.find_element_by_xpath("//input[20]").click()
         self.return_to_home_page()
@@ -87,7 +23,7 @@ class ContactHelper:
         # open first contact editing panel
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
         # fill data
-        self.fill_data(contact)
+        self.fill_contact_form(contact)
         # submit contact editing
         wd.find_element_by_xpath("//input[20]").click()
 
@@ -95,7 +31,6 @@ class ContactHelper:
     def delete_first_contact(self):
         wd = self.app.wd
         self.open_contact_table()
-        #wd.find_element_by_name("selected[]").click()
         self.select_contact()
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         #wd.switch_to.alert.accept()
@@ -114,3 +49,32 @@ class ContactHelper:
     def open_contact_table(self):
         wd = self.app.wd
         wd.get("http://localhost/addressbook/")
+
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).send_keys(text)
+
+    def fill_contact_form(self, contact):
+        self.change_field_value("firstname", contact.firstname)
+        self.change_field_value("lastname", contact.lastname)
+        self.change_field_value("address", contact.address)
+        self.change_field_value("mobile", contact.mobile)
+        self.change_field_value("email", contact.email)
+        self.change_field_value("email2", contact.email2)
+        self.change_field_value("email3", contact.email3)
+        self.change_field_value("nickname", contact.nickname)
+        self.change_field_value("company", contact.company)
+        self.change_field_value("title", contact.title)
+        self.change_field_value("home", contact.home)
+        self.change_field_value("work", contact.work)
+        self.change_field_value("fax", contact.fax)
+        self.change_field_value("homepage", contact.homepage)
+        self.change_field_value("bday", contact.bday)
+        self.change_field_value("bmonth", contact.bmonth)
+        self.change_field_value("byear", contact.byear)
+        self.change_field_value("aday", contact.aday)
+        self.change_field_value("amonth", contact.amonth)
+        self.change_field_value("ayear", contact.ayear)
