c6ac4dafe35d2751a3db64f2bd8b6f3db8fe7054
inyour8bitdreamz18 <WaginElla2019>
2024-08-30 14:40:30 +0200

    Исправлен фильтр для почты и телефона в тесте сверки данных контакта

--- test/test_contact_info.py
+++ test/test_contact_info.py
@@ -7,29 +7,36 @@ def test_info_on_home_page(app):
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.firstname == contact_from_edit_page.firstname.strip()
     assert contact_from_home_page.lastname == contact_from_edit_page.lastname.strip()
+    assert contact_from_home_page.address == contact_from_edit_page.address.strip()
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-    assert clear(contact_from_home_page.address) == clear(contact_from_edit_page.address)
 
 
-def clear_phones(s):
-    return re.sub("[()/ .-]", "", s)
+def clear(phone):
+    if phone != "":
+        return re.sub("[()/ .-]", "", phone)
+    else:
+        return ""
 
-def clear(s):
-    return re.sub("[() -]", "", s)
+
+def clear_emails(email):
+    if email != "":
+        return email.strip()
+    else:
+        return ""
 
 
 def merge_phones_like_on_home_page(contact):
     # Обратная проверка - (работа с данными в функциональном стиле)
     return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear_phones(x),
+                            map(lambda x: clear(x),
                                                    filter(lambda x: x is not None,
                                                                               [contact.home, contact.mobile, contact.work]))))
 
 def merge_emails_like_on_home_page(contact):
     # Обратная проверка - (работа с данными в функциональном стиле)
     return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear(x),
+                            map(lambda x: clear_emails(x),
                                                    filter(lambda x: x is not None,
                                                                               [contact.email, contact.email2, contact.email3]))))
 
