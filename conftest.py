1cdc24c0fc0af215d655c752c86fd976baa7d9e2
inyour8bitdreamz18 <WaginElla2019>
2024-09-18 14:32:57 +0200

    Данные авторизации и baseUrl перенесены в target.json, автом. определяется путь к конфиг. файлу

--- conftest.py
+++ conftest.py
@@ -1,25 +1,32 @@
 import pytest
 from fixture.application import Application
+import json
+import os.path
 
 # Функция, инициализирующая Фикстуру (обязательна метка перед самой функцией)
 # @pytest.fixture(scope="session") убрали scope, чтобы избежать падения браузера
 
 fixture = None
-
+target = None
+# Чтобы не читать config_file при каждом запуске, также как и с fixture, вынесем глобальную переменную target
 @pytest.fixture
 def app(request):
     # Создает Фикстуру
     global fixture
+    global target
     # Через объект request дали доступ к конфигам
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
-    # Пароль админа нужно указывать при запуске и нигде не сохраняется
-    fixture.session.ensure_login(username="admin", password="secret")
+    if target is None:
+        # Сначала Преобразуем в абсолютный путь os.path.abspath(__file__), а затем найдем директорию os.path.dirname()
+        # А потом приклеим наш путь с директорией к конфиг файлу (мы вычисляем путь относительно конфиг файла)
+        # Теперь тесты будут запускаться вне зависимости от рабочей директории
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target["baseUrl"])
+    # Пароль админа нужно указывать при запуске, и он нигде не сохраняется
+    fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
 
 
@@ -38,4 +45,5 @@ def stop(request):
 # Cохранили конфиги для вызова тестов через консоль
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    #parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
